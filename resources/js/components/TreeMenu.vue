0d0d145b25dba9472fe2cba56df2b0afe3e74802
Victor Fokin <victor.fokin@gmail.com>
2025-03-30 23:49:33 +0300

    Class tree alignment

--- resources/js/components/TreeMenu.vue
+++ resources/js/components/TreeMenu.vue
@@ -10,11 +10,13 @@
                 {{ showToggle ? (showChildren ? '−' : '+') : ' ' }}
             </span>
             <input type="checkbox" :value="id" :checked="isChecked" @change="onCheckboxChange" />
-            <span class="node-name">{{ name }}</span>
-            <span class="action-icons" :class="{ 'visible': showIcons }">
-                <span class="add-subclass" @click="openCreateSubclassModal">+</span>
-                <span class="add-object" @click="openCreateObjectModal">📦</span>
-            </span>
+            <div class="node-content">
+                <span class="node-name">{{ name }}</span>
+                <span class="action-icons" :class="{ 'visible': showIcons }">
+                    <span class="add-subclass" @click="openCreateSubclassModal">+</span>
+                    <span class="add-object" @click="openCreateObjectModal">📦</span>
+                </span>
+            </div>
         </div>
         <div class="children" v-if="showChildren">
             <tree-menu
@@ -73,7 +75,7 @@ export default {
             return this.nodes && this.nodes.length > 0;
         },
         indent() {
-            return { marginLeft: `${this.depth * 15}px` }; // Reduced indent, applied to node
+            return { marginLeft: `${this.depth * 15}px` };
         }
     },
     methods: {
@@ -90,38 +92,46 @@ export default {
 <style scoped>
 .tree-menu {
     position: relative;
-    margin-left: 0; /* No extra left margin on the container */
+    margin-left: 0;
 }
 
 .tree-node {
     display: flex;
-    align-items: center;
+    align-items: flex-start; /* Align to top for multi-line consistency */
     padding: 2px 0;
 }
 
 .toggle {
     display: inline-block;
-    width: 16px; /* Slightly smaller to reduce space */
+    width: 16px;
     text-align: center;
     font-size: larger;
     cursor: pointer;
     user-select: none;
+    flex-shrink: 0; /* Prevent shrinking */
 }
 
 input[type="checkbox"] {
-    margin: 0 6px 0 0; /* Reduced spacing */
+    margin: 2px 6px 0 0; /* Align checkbox with first line */
     flex-shrink: 0;
 }
 
+.node-content {
+    display: flex;
+    align-items: flex-start; /* Ensure name and icons align with first line */
+    flex-grow: 1; /* Take up remaining space */
+}
+
 .node-name {
-    /* Removed nowrap and truncation to allow wrapping */
-    margin-right: 5px; /* Space before icons */
+    margin-right: 5px;
+    word-wrap: break-word; /* Allow wrapping */
 }
 
 .action-icons {
     display: inline-flex;
     align-items: center;
     visibility: hidden;
+    flex-shrink: 0; /* Prevent icons from shrinking */
 }
 
 .action-icons.visible {
