a0ddac390c28dc098e14a7162a521ff8b4b6738e
Victor Fokin <victor.fokin@gmail.com>
2025-07-05 01:11:51 +0300

    Acceptance test WIP

--- tests/Acceptance/FirstCest.php
+++ tests/Acceptance/FirstCest.php
@@ -18,5 +18,15 @@ final class FirstCest
     {
         $I->amOnPage('/');
         $I->see('Home');
+        $I->see('Login');
+        $I->amOnPage('/object/d591f9d2-686a-4749-98c3-8fc6bb9d34da'); // Private object
+        $I->see('Login');
+        $I->fillField('email', 'tester@mail.ru');
+        $I->fillField('password', 'qqqqqqq');
+
+        // Submit the form
+        $I->click('button[type="submit"]');
+        $I->dontSee('Login');
+        $I->see('tester');
     }
 }
