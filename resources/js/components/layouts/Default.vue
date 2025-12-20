b12b9c6991221781968ac8a40d106b3835189cd6
Victor Fokin <victor.fokin@gmail.com>
2025-12-21 01:56:06 +0300

    Removed unwanted buttons

--- resources/js/components/layouts/Default.vue
+++ resources/js/components/layouts/Default.vue
@@ -45,16 +45,6 @@
                 </div>
             </div>
         </nav>
-        <nav class="navbar navbar-light bg-light">
-            <div class="container-fluid">
-                <div class="d-flex gap-2">
-                    <button class="btn btn-outline-primary" @click="openCreateModal('Class')">Class</button>
-                    <button class="btn btn-outline-primary" @click="openCreateModal('Person')">Person</button>
-                    <button class="btn btn-outline-primary" @click="openCreateModal('Event')">Event</button>
-                    <button class="btn btn-outline-primary" @click="openCreateModal('Something')">Something</button>
-                </div>
-            </div>
-        </nav>
 
         <!-- Main content area -->
         <main class="mt-3">
