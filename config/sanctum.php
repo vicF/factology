35522f53ad51a44f496b58750a178f77b5d8b07d
Victor Fokin <>
2026-03-23 19:14:22 +0300

    Tests

--- config/sanctum.php
+++ config/sanctum.php
@@ -59,9 +59,4 @@ return [
     |
     */
 
-    'middleware' => [
-        'verify_csrf_token' => App\Http\Middleware\VerifyCsrfToken::class,
-        'encrypt_cookies' => App\Http\Middleware\EncryptCookies::class,
-    ],
-
 ];
