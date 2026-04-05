52de753be3275f9485c8a1b2ce24d8d52a8b41a1
Victor Fokin <>
2026-04-05 15:17:19 +0300

    Tests

--- tests-js/e2e/acceptance/create_delete_classes_test.js
+++ tests-js/e2e/acceptance/create_delete_classes_test.js
@@ -7,6 +7,30 @@ const TEST_USER = {
     password: 'qqqqqqqq'
 };
 
+// Reset database before all tests
+BeforeSuite(async ({ I }) => {
+    console.log('\n========== DATABASE SETUP ==========');
+
+    try {
+        console.log('\n🔄 Running database reset...');
+        const resetResponse = await I.sendPostRequest('/api/test/reset');
+
+        if (resetResponse.data.success) {
+            console.log('✅ Database reset successful!');
+            console.log('📊 Migration Output:', resetResponse.data.output);
+        } else {
+            console.log('❌ Database reset failed:', resetResponse.data.error);
+        }
+
+    } catch (err) {
+        console.log('\n❌ Error accessing test routes:', err.message);
+        console.log('Make sure your test container is running with APP_ENV=testing');
+    }
+
+    console.log('\n====================================\n');
+});
+
+// Login before each test
 Before(async ({ I }) => {
     I.amOnPage('/');
     I.seeElement('a:has-text("Home")');
@@ -28,47 +52,58 @@ Scenario('Create object hierarchy with parent-child relationships', async ({ I }
     await I.createClass('Material Object', 'Physical thing');
     I.see('Material Object');
 
+    // Verify description on the object page
+    I.click('Material Object');
+    I.waitForText('Physical thing', 10);
+    I.see('Physical thing');
+    I.click('Something');
+
     // Create Live being as child of Material Object
     await I.addChildTo('Material Object');
     await I.createClass('Live being', 'Живое существо');
     I.see('Live being');
+
+    // Verify description on the object page
+    I.click('Live being');
+    I.waitForText('Живое существо', 10);
+    I.see('Живое существо');
+    I.click('Something');
 
     // Create Human being as child of Live being
     await I.addChildTo('Live being');
     await I.createClass('Human being', 'Человек');
     I.see('Human being');
 
-    // Verify descriptions are visible
+    // Verify description on the object page
+    I.click('Human being');
+    I.waitForText('Человек', 10);
     I.see('Человек');
-    I.see('Живое существо');
+    I.click('Something');
 
     // Delete Human being
     I.click('Human being');
     I.waitForText('Human being', 10);
     I.click('Delete this object');
-    I.handlePopup({ accept: true });
+    I.acceptPopup();
     I.waitForText('Link', 15);
     I.dontSee('Human being');
-    I.dontSee('Человек');
 
     // Delete Live being
     I.click('Live being');
     I.waitForText('Live being', 10);
     I.click('Delete this object');
-    I.handlePopup({ accept: true });
+    I.acceptPopup();
     I.waitForText('Link', 15);
     I.dontSee('Live being');
-    I.dontSee('Живое существо');
     I.see('Material Object');
 
     // Delete Material Object
     I.click('Material Object');
     I.waitForText('Material Object', 10);
     I.click('Delete this object');
-    I.handlePopup({ accept: true });
+    I.acceptPopup();
     I.waitForText('Link', 15);
     I.dontSee('Material Object');
-    I.dontSee('Physical thing');
 });
 
 Scenario('Verify deleted objects are not visible in search', async ({ I }) => {
@@ -76,15 +111,19 @@ Scenario('Verify deleted objects are not visible in search', async ({ I }) => {
     await I.addChildTo('Something');
     await I.createClass('Temp Object To Delete', 'This will be deleted');
     I.see('Temp Object To Delete');
-    I.see('This will be deleted');
 
-    // Delete it
+    // Verify description on object page
+    I.click('Temp Object To Delete');
+    I.waitForText('This will be deleted', 10);
+    I.see('This will be deleted');
+    I.click('Something');
+
+    // Delete the object
     I.click('Temp Object To Delete');
     I.waitForText('Temp Object To Delete', 10);
     I.click('Delete this object');
-    I.handlePopup({ accept: true });
+    I.acceptPopup();
 
     I.waitForText('Link', 15);
     I.dontSee('Temp Object To Delete');
-    I.dontSee('This will be deleted');
 });
