cf8fffe6f099f05177555a83b1b3097b354dc3ae
Victor Fokin <>
2026-04-05 18:43:17 +0300

    tests

--- tests-js/e2e/acceptance/create_delete_classes_test.js
+++ tests-js/e2e/acceptance/create_delete_classes_test.js
@@ -17,14 +17,12 @@ BeforeSuite(async ({ I }) => {
 
         if (resetResponse.data.success) {
             console.log('✅ Database reset successful!');
-            console.log('📊 Migration Output:', resetResponse.data.output);
         } else {
             console.log('❌ Database reset failed:', resetResponse.data.error);
         }
 
     } catch (err) {
         console.log('\n❌ Error accessing test routes:', err.message);
-        console.log('Make sure your test container is running with APP_ENV=testing');
     }
 
     console.log('\n====================================\n');
@@ -50,40 +48,49 @@ Scenario('Create object hierarchy with parent-child relationships', async ({ I }
     // Create Material Object
     await I.addChildTo('Something');
     await I.createClass('Material Object', 'Physical thing');
-    I.see('Material Object');
 
-    // Verify description on the object page
+    // The name might be truncated to "Ma" in the tree view
+    // So check for partial text or the link itself
+    I.seeElement('a:has-text("Material Object")');
+    I.see('Material Object'); // This might fail if truncated
+
+    // Alternative: Check that the object was created by clicking on it
     I.click('Material Object');
     I.waitForText('Physical thing', 10);
     I.see('Physical thing');
+    I.see('Material Object'); // Full name should be on the detail page
     I.click('Something');
 
     // Create Live being as child of Material Object
     await I.addChildTo('Material Object');
     await I.createClass('Live being', 'Живое существо');
+    I.seeElement('a:has-text("Live being")');
     I.see('Live being');
 
     // Verify description on the object page
     I.click('Live being');
     I.waitForText('Живое существо', 10);
     I.see('Живое существо');
+    I.see('Live being');
     I.click('Something');
 
     // Create Human being as child of Live being
     await I.addChildTo('Live being');
     await I.createClass('Human being', 'Человек');
+    I.seeElement('a:has-text("Human being")');
     I.see('Human being');
 
     // Verify description on the object page
     I.click('Human being');
     I.waitForText('Человек', 10);
     I.see('Человек');
+    I.see('Human being');
     I.click('Something');
 
     // Delete Human being
     I.click('Human being');
     I.waitForText('Human being', 10);
-    I.click('Delete this object');
+    I.click('Delete');
     I.acceptPopup();
     I.waitForText('Link', 15);
     I.dontSee('Human being');
@@ -91,7 +98,7 @@ Scenario('Create object hierarchy with parent-child relationships', async ({ I }
     // Delete Live being
     I.click('Live being');
     I.waitForText('Live being', 10);
-    I.click('Delete this object');
+    I.click('Delete');
     I.acceptPopup();
     I.waitForText('Link', 15);
     I.dontSee('Live being');
@@ -100,7 +107,7 @@ Scenario('Create object hierarchy with parent-child relationships', async ({ I }
     // Delete Material Object
     I.click('Material Object');
     I.waitForText('Material Object', 10);
-    I.click('Delete this object');
+    I.click('Delete');
     I.acceptPopup();
     I.waitForText('Link', 15);
     I.dontSee('Material Object');
@@ -109,20 +116,22 @@ Scenario('Create object hierarchy with parent-child relationships', async ({ I }
 Scenario('Verify deleted objects are not visible in search', async ({ I }) => {
     // Create a temporary object
     await I.addChildTo('Something');
-    await I.createClassSimple('Temp Object To Delete', 'This will be deleted');
 
-    // Go to Something page to see the new object
+    I.waitForElement('input[name="name"]', 10);
+    I.fillField('input[name="name"]', 'Temp Object To Delete');
+    I.fillField('input[name="description"]', 'This will be deleted');
+    I.click('Save');
+    I.wait(2);
+
     I.click('Something');
     I.waitForText('Temp Object To Delete', 15);
     I.see('Temp Object To Delete');
 
-    // Verify description on object page
     I.click('Temp Object To Delete');
     I.waitForText('This will be deleted', 10);
     I.see('This will be deleted');
     I.click('Something');
 
-    // Delete the object
     I.click('Temp Object To Delete');
     I.waitForText('Temp Object To Delete', 10);
     I.click('Delete');
