4376d4735dcbeb94fe162c8cd1e1df297d88d3de
Victor Fokin <victor.fokin@gmail.com>
2026-04-05 14:53:19 +0300

    Tests

--- tests-js/e2e/acceptance/create_delete_classes_test.js
+++ tests-js/e2e/acceptance/create_delete_classes_test.js
@@ -1,16 +1,13 @@
 // tests-js/e2e/acceptance/create_delete_classes_test.js
 Feature('Object Hierarchy Management');
 
-// Default test user
 const TEST_USER = {
     name: 'Test User',
     email: 'test@test.com',
     password: 'qqqqqqqq'
 };
 
-// Login before test
 Before(async ({ I }) => {
-    // Login with default test user
     I.amOnPage('/');
     I.seeElement('a:has-text("Home")');
 
@@ -22,135 +19,72 @@ Before(async ({ I }) => {
     I.fillField('Password', TEST_USER.password);
     I.click('Log in');
 
-    // Verify login success - increase timeout to 15 seconds
     I.waitForText(TEST_USER.name, 15);
 });
 
 Scenario('Create object hierarchy with parent-child relationships', async ({ I }) => {
-    // ========== Create Material Object ==========
-    // Add child to Something
+    // Create Material Object
     await I.addChildTo('Something');
-
-    I.waitForElement('input[name="name"]', 10);
-    I.seeInField('Something', 'Something');
-    I.fillField('input[name="name"]', 'Material Object');
-    I.fillField('input[name="description"]', 'Physical thing');
-    I.click('Save');
-
-    I.waitForText('Material Object', 15);
+    await I.createClass('Material Object', 'Physical thing');
     I.see('Material Object');
 
-    // Add child to Material Object
+    // Create Live being as child of Material Object
     await I.addChildTo('Material Object');
-
-    I.waitForElement('input[name="name"]', 10);
-    I.fillField('input[name="name"]', 'Live being');
-    I.fillField('input[name="description"]', 'Живое существо');
-    I.click('Save');
-
-    I.waitForText('Live being', 15);
+    await I.createClass('Live being', 'Живое существо');
     I.see('Live being');
 
-    // Add child to Live being
+    // Create Human being as child of Live being
     await I.addChildTo('Live being');
-
-    I.waitForElement('input[name="name"]', 10);
-    I.fillField('input[name="name"]', 'Human being');
-    I.fillField('input[name="description"]', 'Человек');
-    I.click('Save');
-
-    I.waitForText('Human being', 15);
+    await I.createClass('Human being', 'Человек');
     I.see('Human being');
-    I.see('Description: Человек');
 
-    // ========== Verify hierarchy in search results ==========
-    I.see('Description: Живое существо');
-    I.see('Description: Человек');
-
-    // ========== Delete Human being and verify it's gone ==========
-    // Click on Human being to go to its page
-    I.click('Human being');
-    I.waitForText('Human being', 10);
+    // Verify descriptions are visible
+    I.see('Человек');
+    I.see('Живое существо');
 
     // Delete Human being
+    I.click('Human being');
+    I.waitForText('Human being', 10);
     I.click('Delete this object');
-
-    // Handle confirmation dialog
-    try {
-        I.acceptPopup();
-    } catch (err) {
-        // If popup handling fails, try clicking confirm button
-        I.click('Confirm');
-    }
-
+    I.handlePopup({ accept: true });
     I.waitForText('Link', 15);
     I.dontSee('Human being');
-    I.dontSee('Description: Человек');
-
-    // ========== Delete Live being and verify it's gone ==========
-    // Go back to Live being
-    I.click('Live being');
-    I.waitForText('Live being', 10);
+    I.dontSee('Человек');
 
     // Delete Live being
+    I.click('Live being');
+    I.waitForText('Live being', 10);
     I.click('Delete this object');
-
-    try {
-        I.acceptPopup();
-    } catch (err) {
-        I.click('Confirm');
-    }
-
-    // Verify Live being is gone (but Material Object should remain)
+    I.handlePopup({ accept: true });
     I.waitForText('Link', 15);
     I.dontSee('Live being');
-    I.dontSee('Description: Живое существо');
-    I.see('Material Object'); // Parent should still exist
-
-    // ========== Verify Material Object still exists ==========
-    I.click('Material Object');
-    I.waitForText('Material Object', 10);
+    I.dontSee('Живое существо');
     I.see('Material Object');
 
-    // ========== Delete Material Object ==========
+    // Delete Material Object
+    I.click('Material Object');
+    I.waitForText('Material Object', 10);
     I.click('Delete this object');
-
-    try {
-        I.acceptPopup();
-    } catch (err) {
-        I.click('Confirm');
-    }
-
-    // Verify Material Object is gone
+    I.handlePopup({ accept: true });
     I.waitForText('Link', 15);
     I.dontSee('Material Object');
     I.dontSee('Physical thing');
 });
 
 Scenario('Verify deleted objects are not visible in search', async ({ I }) => {
-    // First, create a test object
-    I.click('button:has-text("+")');
-    I.waitForElement('input[name="name"]', 10);
-    I.fillField('input[name="name"]', 'Temp Object To Delete');
-    I.fillField('input[name="description"]', 'This will be deleted');
-    I.click('Save');
-    I.waitForText('Temp Object To Delete', 15);
-
-    // Verify it appears
+    // Create a temporary object
+    await I.addChildTo('Something');
+    await I.createClass('Temp Object To Delete', 'This will be deleted');
     I.see('Temp Object To Delete');
-    I.see('Description: This will be deleted');
+    I.see('This will be deleted');
 
-    // Delete the object
+    // Delete it
+    I.click('Temp Object To Delete');
+    I.waitForText('Temp Object To Delete', 10);
     I.click('Delete this object');
+    I.handlePopup({ accept: true });
 
-    try {
-        I.acceptPopup();
-    } catch (err) {
-        I.click('Confirm');
-    }
-
-    // Wait for deletion and verify it's gone
     I.waitForText('Link', 15);
     I.dontSee('Temp Object To Delete');
-    I.dontSee('Description: This will be deleted');
+    I.dontSee('This will be deleted');
 });
