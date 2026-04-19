d3e45f516eba5ddea7a17de126feb2d08677a09d
Victor Fokin <>
2026-04-19 23:16:33 +0300

    Full tree

--- resources/js/components/ClassTree.vue
+++ resources/js/components/ClassTree.vue
@@ -2,14 +2,27 @@
     <div>
         Classes:
         <div v-if="rootNodes && rootNodes.length">
-            <TreeMenu
-                v-for="root in rootNodes"
-                :key="root.id"
-                :id="root.id"
-                :name="root.name"
-                :nodes="root.nodes || []"
-                :depth="0"
-            />
+            <template v-for="root in rootNodes" :key="root.id">
+                <!-- If this is the "Anything" node, render its children directly -->
+                <template v-if="root.id === '939cd822-9e23-450c-8c5e-c23f67cca792' || root.name === 'Anything'">
+                    <TreeMenu
+                        v-for="child in root.nodes"
+                        :key="child.id"
+                        :id="child.id"
+                        :name="child.name"
+                        :nodes="child.nodes || []"
+                        :depth="0"
+                    />
+                </template>
+                <!-- Otherwise render the node normally -->
+                <TreeMenu
+                    v-else
+                    :id="root.id"
+                    :name="root.name"
+                    :nodes="root.nodes || []"
+                    :depth="0"
+                />
+            </template>
         </div>
         <div v-else class="text-muted p-3">
             No classes available
