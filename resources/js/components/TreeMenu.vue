e5f4512e6281ce5ca917aab22d9b6cf434bf4314
Victor Fokin <victor.fokin@gmail.com>
2026-05-10 16:14:48 +0300

    Layouts

--- resources/js/components/TreeMenu.vue
+++ resources/js/components/TreeMenu.vue
@@ -16,9 +16,13 @@
                 :node-id="id"
                 width="18px"
                 style="padding-right: 4px"
-            /><div class="node-content">
-                <span class="node-name"><router-link class="dropdown-item" :to="`/object/${id}`">{{ name }}</router-link></span>
-                <span  class="action-icons" :class="{ 'visible': authenticated && showIcons }">
+            />
+            <div class="node-content">
+                <span class="node-name">
+                    <router-link class="dropdown-item" :to="`/object/${id}`">{{ name }}</router-link>
+                    <span v-if="isPrivate" class="private-icon" title="Private"><IconPrivate v-if="isPrivate" /></span>
+                </span>
+                <span class="action-icons" :class="{ 'visible': authenticated && showIcons }">
                     <span class="add-subclass" @click="openCreateSubclassModal" :title="`Add child class below &quot;${name}&quot;`">+</span>
                     <span class="add-object" @click="openCreateObjectModal" :title="`Create object of class &quot;${name}&quot;`">📦</span>
                 </span>
@@ -33,6 +37,7 @@
                 :name="node.name"
                 :depth="depth + 1"
                 :checked-items="checkedItems"
+                :public="node.public"
                 @update-checked="handleCheckedUpdate"
             ></tree-menu>
         </div>
@@ -43,12 +48,14 @@
 import { ref, computed } from 'vue';
 import { useSearchStore } from '../stores/search';
 import { eventBus } from '../eventBus';
-import {LINK_TO_CLASS, THING_TYPE, CLASS_TYPE, LINK_TO_PARENT} from '../constants.js';
-import {useAuthStore} from "../stores/auth";
+import { LINK_TO_CLASS, THING_TYPE, CLASS_TYPE, LINK_TO_PARENT } from '../constants.js';
+import { useAuthStore } from "../stores/auth";
 import Image from "./Image.vue";
+import {IconPrivate} from "./icons";
 
-const authStore    = useAuthStore()
-const authenticated = computed(() => authStore.authenticated)
+const authStore = useAuthStore();
+const authenticated = computed(() => authStore.authenticated);
+
 // Props definition
 const props = defineProps({
     id: {
@@ -70,18 +77,18 @@ const props = defineProps({
     checkedItems: {
         type: Array,
         default: () => []
+    },
+    public: {
+        type: Number,
+        default: 1          // 1 = public, 0 = private
     }
 });
 
 // Emits definition
 const emit = defineEmits(['update-checked']);
 
-// Component name
-defineOptions({
-    name: 'tree-menu'
-});
+defineOptions({ name: 'tree-menu' });
 
-// Store
 const store = useSearchStore();
 
 // State
@@ -89,17 +96,10 @@ const showChildren = ref(true);
 const showIcons = ref(false);
 
 // Computed
-const isChecked = computed(() => {
-    return store.checkedItems.includes(props.id);
-});
-
-const showToggle = computed(() => {
-    return props.nodes && props.nodes.length > 0;
-});
-
-const indent = computed(() => {
-    return { marginLeft: `${props.depth * 15}px` };
-});
+const isChecked = computed(() => store.checkedItems.includes(props.id));
+const showToggle = computed(() => props.nodes && props.nodes.length > 0);
+const indent = computed(() => ({ marginLeft: `${props.depth * 15}px` }));
+const isPrivate = computed(() => props.public === 0);
 
 // Methods
 const toggleChildren = () => {
@@ -115,72 +115,55 @@ const onCheckboxChange = () => {
 // Create a subclass (Class type)
 const openCreateSubclassModal = () => {
     console.log('TreeMenu.vue - Creating subclass of:', props.name);
-
-    // Create initial linked objects for the new class
     const initialLinkedObjects = [];
-
-    // Link to parent class
     if (props.id) {
         initialLinkedObjects.push({
             one_thing_id: props.id,
-            link_type_id: LINK_TO_PARENT, // Use the constant from constants.js
+            link_type_id: LINK_TO_PARENT,
             description: `Subclass of ${props.name}`,
         });
     }
-
     const payload = {
         title: `Create Subclass of "${props.name}"`,
-        params: {
-            type: CLASS_TYPE, // 2 - Class type
-        },
-        initialLinkedObjects: initialLinkedObjects,
+        params: { type: CLASS_TYPE },
+        initialLinkedObjects,
         callback: {
             type: 'class-created',
             parentId: props.id,
             parentName: props.name
         }
     };
-
-    console.log('TreeMenu.vue - Emitting open-create-modal for subclass:', payload);
     eventBus.emit('open-create-modal', payload);
 };
 
 // Create an object of this class (Thing type)
 const openCreateObjectModal = () => {
     console.log('TreeMenu.vue - Creating object of class:', props.name);
-
-    // Create initial linked objects for the new object
     const initialLinkedObjects = [];
-
-    // Link to the class
     if (props.id) {
         initialLinkedObjects.push({
             other_thing_id: props.id,
-            link_type_id: LINK_TO_CLASS, // Link to class relationship
+            link_type_id: LINK_TO_CLASS,
             description: `Object of class ${props.name}`,
         });
     }
-
     const payload = {
         title: `Create Object of Class "${props.name}"`,
         params: {
-            type: THING_TYPE, // 3 - Thing type
+            type: THING_TYPE,
             classId: props.id,
             className: props.name,
         },
-        initialLinkedObjects: initialLinkedObjects,
+        initialLinkedObjects,
         callback: {
             type: 'object-created',
             classId: props.id,
             className: props.name
         }
     };
-
-    console.log('TreeMenu.vue - Emitting open-create-modal for object:', payload);
     eventBus.emit('open-create-modal', payload);
 };
 
-// Handle checked update from child components
 const handleCheckedUpdate = (checkedItems) => {
     emit('update-checked', checkedItems);
 };
@@ -192,13 +175,11 @@ const handleCheckedUpdate = (checkedItems) => {
     margin-left: 0;
     padding-left: 0;
 }
-
 .tree-node {
     display: flex;
     align-items: center;
     padding: 1px 0;
 }
-
 .toggle {
     display: flex;
     align-items: center;
@@ -210,47 +191,42 @@ const handleCheckedUpdate = (checkedItems) => {
     user-select: none;
     flex-shrink: 0;
 }
-
 input[type="checkbox"] {
     margin: 0 6px 0 0;
     width: 16px;
     height: 16px;
     flex-shrink: 0;
 }
-
 .node-content {
     display: flex;
     align-items: center;
     flex-grow: 1;
 }
-
 .node-name {
     margin-right: 5px;
     word-wrap: break-word;
     line-height: 1.1;
+    display: inline-flex;
+    align-items: center;
+    gap: 4px;
 }
-
 .action-icons {
     display: inline-flex;
     align-items: center;
     visibility: hidden;
     flex-shrink: 0;
 }
-
 .action-icons.visible {
     visibility: visible;
 }
-
 .add-subclass, .add-object {
     cursor: pointer;
     font-size: larger;
     margin-left: 5px;
 }
-
 .add-subclass:hover, .add-object:hover {
     color: #007bff;
 }
-
 .children {
     padding-left: 0;
 }
