fe5925fd00ff87c4b89c7895172210de5f032b89
Victor Fokin <victor.fokin@gmail.com>
2026-07-07 14:33:10 +0300

    tests

--- vitest.config.js
+++ vitest.config.js
@@ -2,6 +2,9 @@
 import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
     plugins: [vue()],
@@ -12,11 +15,16 @@ export default defineConfig({
         }
     },
     test: {
+        root: path.resolve(__dirname),
+        pool: 'threads',
         globals: true,
         environment: 'jsdom',
         setupFiles: [
-            './tests-vitest/setup.js',
-            './tests-vitest/setup.localDb.js',
+            path.resolve(__dirname, 'tests-vitest/setup.js'),
+            path.resolve(__dirname, 'tests-vitest/setup.localDb.js'),
+        ],
+        include: [
+            path.resolve(__dirname, 'tests-vitest/**/*.{test,spec}.?(c|m)[jt]s?(x)'),
         ],
         server: {
             deps: {
